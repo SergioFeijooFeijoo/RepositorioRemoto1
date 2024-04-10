194dea86d83a3e9dd7ddaf7f5fdf55856b9ce189
mañana <maana@A23-24>
2024-04-10 13:26:58 +0200

    Segunda confirmacion.

--- proyecto1/src/paquete1/Principal.java
+++ proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 
 		System.out.println("Estamos haciendo control de versiones de Git.");
+		System.out.println("Cambio para probar historial.");
 	}
 
 }
